d0db1de9036a4027b159784ef40627a3539c739c
darkwizard41 <66963415+darkwizard41@users.noreply.github.com>
2020-09-23 13:58:12 -0400

    Add files via upload

---
+++ packages.py
@@ -0,0 +1,7 @@
+import dopamine
+import gym
+import gym-retro as retro
+
+print("dopamine version: " + dopamine.__version)
+print("gym-retro version: " + gym.__version)
+print("retro: " + retro.__version)
