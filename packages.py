daed964976e077b12509f22664174ca4c6cb7efe
Siyuan “Kevin” Peng <48134935+syKevinPeng@users.noreply.github.com>
2020-09-28 16:20:58 -0400

    Delete packages.py

--- packages.py
+++
@@ -1,7 +0,0 @@
-import dopamine
-import gym
-import gym-retro as retro
-
-print("dopamine version: " + dopamine.__version)
-print("gym-retro version: " + gym.__version)
-print("retro: " + retro.__version)
